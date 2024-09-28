d3c8785b3f97bf77cf2349325bb291608d9fdcfb
ruhamabek <ruhamauzubo@gmail.com>
2024-09-28 15:39:06 +0300

    creating a model for the list items

---
+++ to-do-list-ts/src/model/ListItem.ts
@@ -0,0 +1,37 @@
+export interface Item {
+      id: string,
+      item: string,
+      checked: boolean
+}
+
+class ListItems implements Item {
+       
+  constructor(
+    private _id = '',
+    private _item = '',
+   private _checked = false ){}
+
+   get id(): string {
+      return this._id
+   }
+
+   set id(id: string){
+       this._id = id;
+   }
+  
+   get item(): string {
+       return this._item;
+   }
+
+   set item(item: string){
+       this._item = item;
+   }
+
+   get checked():boolean{
+        return this._checked
+   }
+
+   set checked(checked: boolean){
+       this._checked = checked;
+   }
+}
